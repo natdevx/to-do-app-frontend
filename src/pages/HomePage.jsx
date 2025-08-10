951a60d53f460a9dea4937722b7f49ad16993d19
Nat Arrazola <NatyArrazola13@gmail.com>
2025-08-10 13:19:26 -0600

    Modificacion en el homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,40 +2,42 @@ import { useEffect, useState } from "react";
 import CardNote from "../components/CardNote";
 import axios from "axios";
 import formatData from "../utils/formatDate";
-
 const apiURL = import.meta.env.VITE_API_URL;
 
 const HomePage = () => {
-  const [ notes, setNotes] = useState([]);
+  const [notes, setNotes] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await axios.get(`${apiURL}/api/notes`);
-        setNotes(response.data)
-        setLoading(false)
-        console.log(response);
+        setNotes(response.data);
+        setLoading(false);
       } catch (error) {
         console.log(error);
       }
     };
 
-    fetchData()
+    fetchData();
   }, []);
 
-  if (loading) return <span>Cargando...</span>
+  const handleDelete = (id) => {
+    setNotes((prevNotes) => prevNotes.filter((note) => note._id !== id));
+  };
+
+  if (loading) return <span>Cargando...</span>;
 
   return (
-    <div className="grid grid-cols-[repeat(auto-fit,_minmax(280px,_1fr))] gap-4 xl:grid-cols-[repeat
-    (auto-fit,_minmax(350px,_1fr))]">
-      {notes.map(note => (
+    <div className="grid grid-cols-[repeat(auto-fit,_minmax(280px,_1fr))] gap-4 xl:grid-cols-[repeat(auto-fit,_minmax(350px,_1fr))]">
+      {notes.map((note) => (
         <CardNote
           key={note._id}
           title={note.title}
           description={note.description}
           id={note._id}
           date={formatData(note.createdAt)}
+          onDelete={handleDelete}
         />
       ))}
     </div>
